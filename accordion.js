95f7eb4afba14cbbe8bf9ec8a9f708bcaf877639
seant@bgsu.edu <54330446+softwaresean@users.noreply.github.com>
2020-03-09 17:32:26 -0400

    Refactor

--- accordion.js
+++ accordion.js
@@ -2,18 +2,15 @@ let panes = null;
 panes = document.querySelectorAll('.ds-pane');
 
 function calculateGeometries(panes) {
-  debugger
   if (panes.length === 0) return;
-  let baseY = 0;
   panes.forEach((pane, index) => {
-    const headerHeight = pane.querySelector('button[role="tab"]').offsetHeight;
-    const availableHeight = pane.offsetHeight = (panes.length * headerHeight)
-    const paneContent = pane.querySelector('.content');
-    pane.style.transform = `translateY(${baseY + (headerHeight * index)}px)`;
-    paneContent.style.height = `${availableHeight}px`;
-    if(pane.getAttribute('aria-expanded')) {
-      baseY = availableHeight;
-    }
+    const panelBtn = pane.querySelector('button');
+    panelBtn.addEventListener('click', () => {
+      const dspane = panelBtn.parentElement;
+      dspane.getAttribute('aria-expanded') == 'true' ?
+      dspane.setAttribute('aria-expanded', false) :
+      dspane.setAttribute('aria-expanded', true)
+    });
   });
 }
 calculateGeometries(panes);
