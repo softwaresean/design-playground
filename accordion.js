c471d4be14484f6d79df61ac6b8deea7dd2ba6e1
seant@bgsu.edu <54330446+softwaresean@users.noreply.github.com>
2020-03-10 14:19:38 -0400

    Refactor.

--- accordion.js
+++ accordion.js
@@ -1,16 +1,17 @@
 const panes = document.querySelectorAll('.ds-pane');
 
-function calculateGeometries(panes) {
+function panelEventHandler(panes) {
   if (panes.length === 0) return;
   panes.forEach(pane => {
     const panelBtn = pane.querySelector('button');
-    panelBtn.addEventListener('click', () => {
-      const dspane = panelBtn.parentElement;
-      dspane.getAttribute('aria-expanded') == 'true' ?
-      dspane.setAttribute('aria-expanded', false) :
-      dspane.setAttribute('aria-expanded', true)
-    });
+    panelBtn.addEventListener('click', setPanelAttr);
   });
 }
 
-calculateGeometries(panes);
+function setPanelAttr() {
+  const panel = this.parentElement;
+  const panelAttrBool = panel.getAttribute('aria-expanded') == 'true' ? true : false;
+  return panel.setAttribute('aria-expanded', !panelAttrBool);
+}
+
+panelEventHandler(panes);
